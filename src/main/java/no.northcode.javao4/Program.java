6bcc81501028bcffc98c71e3992696d8e3e81dfe
Andreas Larsen <northcode.no@gmail.com>
2016-04-20 14:24:26 +0200

    Initial Commit

---
+++ src/main/java/no.northcode.javao4/Program.java
@@ -0,0 +1,10 @@
+package no.northcode.javao4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Program {
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+    }
+}
