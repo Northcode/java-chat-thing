16497f6dc87bdf58ae8a241db20df703289efa03
Andreas Larsen <northcode.no@gmail.com>
2016-11-26 19:07:44 +0100

    Added some comments to explain GenSig

--- src/main/java/no.northcode.chatclient/GenSig.java
+++ src/main/java/no.northcode.chatclient/GenSig.java
@@ -21,42 +21,56 @@ public class GenSig {
 	    System.err.println("Usage: sign keystorePath:keystorePass keyAlias KeyPassword file");
 	} else try {
 
+		// get keystore args
 		String[] keystore_parts = args[1].split(":");
 		String keystore_path  = keystore_parts[0];
 		String keystore_pass  = keystore_parts[1];
 
+
+		// get keystore instance
 		KeyStore ks = KeyStore.getInstance("JKS");
 
+		// check if keystore exists and load if it does
 		if (! new File(keystore_path).exists()) {
 		    System.err.println("Cannot find keystore at path: " + keystore_path);
 		} else {
 		    ks.load(new FileInputStream(keystore_path), keystore_pass.toCharArray());
 		}
 
+		
+		// get key args
 		String alias = args[2];
 		String password = args[3];
 
+		// try to load key from keystore
 		Key privkey = ks.getKey(alias, password.toCharArray());
+		// load public key from keystore
 		PublicKey pubkey = ks.getCertificate(alias).getPublicKey();
 
+		// get a signature instance of type SHA1 with RSA
 		Signature rsa = Signature.getInstance("SHA1withRSA");
+
+		// initialize signing with private key privkey
 		rsa.initSign((PrivateKey)privkey);
 
+		// open file to sign for reading
 		FileInputStream fis = new FileInputStream(args[4]);
 		BufferedInputStream bufin = new BufferedInputStream(fis);
 
 		byte[] buffer = new byte[1024];
 		int len;
 	
+		// read and add file contents to signature
 		while ((len = bufin.read(buffer)) >= 0) {
 		    rsa.update(buffer, 0, len);
 		};
 
 		bufin.close();
 
+		// get signature bytes from signing object
 		byte[] realSig = rsa.sign();
 
-		/* save the signature in a file */
+		/* save the signature bytes in a file */
 		FileOutputStream sigfos = new FileOutputStream(args[4] + ".sig");
 		sigfos.write(realSig);
 		sigfos.close();
